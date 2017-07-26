c87eaf6439aa5ec20dc8c38b81935ea31f2a67bf
surya <suryamuppalla@gmail.com>
2017-07-26 20:59:58 +0530

    Deal price DIV needs design as like couponzeta.in Gradients Related stores design need to be like Coupondunia.in stores Go to Top button in website H2 tag in capitalize in all pages

--- static/development/customer-panel/modules/app.js
+++ static/development/customer-panel/modules/app.js
@@ -15,6 +15,18 @@ angular.module('APP', ['ui.router', 'oc.lazyLoad', 'ngSanitize'])
         $rootScope.$on('$stateChangeSuccess', function() {
             document.body.scrollTop = document.documentElement.scrollTop = 0;
         });
+
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) {
+                $('.goToTop').fadeIn();
+            } else {
+                $('.goToTop').fadeOut();
+            }
+        });
+        $('.goToTop').click(function () {
+            $("html, body").animate({ scrollTop: 0 }, 100);
+            return false;
+        });
     }])
     .config(['$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider', '$locationProvider',
         function($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, $locationProvider) {
