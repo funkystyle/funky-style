c8a2ca0861f601879b0209a58ca65de81b470525
satya <c_sveges@qti.qualcomm.com>
2017-08-21 08:51:56 +0530

    fixed delete user

--- fab/users/controllers.py
+++ fab/users/controllers.py
@@ -1,13 +1,14 @@
 from flask import  request, abort, jsonify
+from bson.objectid import ObjectId
 from fab import admin_login_required
 from fab import app
 from settings import LOGGER,  COLLECTION_NAMES
 
 #@admin_login_required
-@app.route('/api/1.0/delete-documents', methods=['GET', 'DELETE'])
+@app.route('/api/1.0/delete-documents', methods=['GET', 'DELETE', 'POST'])
 def bulk_delete():
-        input_payload = request.json
-
+    results = []
+    for input_payload in request.json:
         if 'resource_name' not in input_payload:
             LOGGER.error("resource name not found in input payload")
             abort(400, "resource name  not found in input payload")
@@ -20,16 +21,23 @@ def bulk_delete():
 
         if input_payload['resource_name'] not in COLLECTION_NAMES:
             abort(400, 'RESOURCE NAME SHOULD BE IN:{0}'.format(COLLECTION_NAMES))
-
+        message = {}
+        LOGGER.info("resource name:{}".format(input_payload['resource_name']))
         accounts = app.data.driver.db[input_payload['resource_name']]
-        results = []
+        message['resource_name'] = input_payload['resource_name']
         for id in input_payload['ids']:
-            message = {}
             message[id] = False
-            data = accounts.remove({"_id": "34234234"})
-            if data['n']:
-                message[id] = True
-            results.append(message)
-        response = jsonify(errors=[], data=results)
-        response.status_code = 204
-        return response
+            LOGGER.info(id)
+            find_account = accounts.find_one({"_id": ObjectId(id)})
+            if not find_account:
+                message[id] = False
+                message['message'] = "{} not found".format(id)
+            else:
+                data = accounts.remove({"_id": ObjectId(id)})
+                if data['n']:
+                    message[id] = True
+                    message['message'] = "successfully deleted."
+        results.append(message)
+    response = jsonify(errors=[], data=results)
+    response.status_code = 200
+    return response
