5eb07b29a643e31900ee30e362f43bfdd99ab2ed
surya <suryamuppalla@gmail.com>
2017-09-27 09:50:32 +0530

    Deeplink HTTP issue fixed

--- static/app/admin-panel/modules/users/all/all.users.module.js
+++ static/app/admin-panel/modules/users/all/all.users.module.js
@@ -102,6 +102,8 @@ angular.module("allUsersModule", ['constantModule', 'toastr', 'personFactoryModu
         delete $scope.u_user._created;
         delete $scope.u_user._links;
         delete $scope.u_user._updated;
+        delete $scope.u_user.password.password_raw;
+        delete $scope.u_user.password.password;
         if($scope.password.password_raw) {
             $scope.u_user.password.password_raw = $scope.password.password_raw;
         }
@@ -141,16 +143,4 @@ angular.module("allUsersModule", ['constantModule', 'toastr', 'personFactoryModu
             toastr.error(error.data._error.message, 'Error!');
         });
     };
-
-    $scope.pageChangeHandler = function(num) {
-        console.log('drinks page changed to ' + num);
-    };
-
-    $scope.toggleSidebar = function(id) {
-        if ($("#"+id).css("right") == "0px") {
-            $("#"+id).animate({ "right": '-1000', 'display': 'none' }, 500);
-        } else {
-            $("#"+id).animate({ "right": '0', 'display': 'block' }, 500);
-        }
-    }
 });
